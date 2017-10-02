9684e99cabc5cddfc48fcc2559f34ea84d86ae19
pgkuroneru <pgkuroneru@users.noreply.github.com>
2017-10-02 17:51:28 +0900

    Fix App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,10 +2,10 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		try {
-			System.out.println("Hello " + args[0] + "!");
-		} catch (ArrayIndexOutOfBoundsException e) {
+		if (args == null || args.length == 0) {
 			System.out.println("Hello!");
+		} else {
+			System.out.println("Hello " + args[0] + "World!");
 		}
 	}
 }
