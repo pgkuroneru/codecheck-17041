b249f987cf7a6504245c3e05576777e4d7d8076c
pgkuroneru <pgkuroneru@users.noreply.github.com>
2017-10-02 16:59:26 +0900

    Update App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,10 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		if (args[0] == "") {
+			System.out.println("Hello!");
+		} else {
+			System.out.println("Hello " + args[0] + "!");
 		}
 	}
 }
