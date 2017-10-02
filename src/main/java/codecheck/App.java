2159add4290f2ad1ab05702d916fe6b6aa1b088e
pgkuroneru <pgkuroneru@users.noreply.github.com>
2017-10-02 17:56:15 +0900

    Fix App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,10 +2,10 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		if (args == null || args.length == 0) {
+		if (args.equals(null) || args.length == 0 || args[0] == "") {
 			System.out.println("Hello!");
 		} else {
-			System.out.println("Hello " + args[0] + "World!");
+			System.out.println("Hello " + args[0] + "!");
 		}
 	}
 }
