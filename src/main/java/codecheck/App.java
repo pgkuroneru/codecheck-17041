7a7c38a0916882b44ee68b8fa27526db07d8cc7e
pgkuroneru <pgkuroneru@users.noreply.github.com>
2017-10-02 17:35:38 +0900

    Fix App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,7 +2,7 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		if (args.length == 0) {
+		if (args.length != 1) {
 			System.out.println("Hello!");
 		} else {
 			System.out.println("Hello " + args[0] + "!");
